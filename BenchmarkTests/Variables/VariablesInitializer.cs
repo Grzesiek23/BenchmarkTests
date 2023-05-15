6958ad5350fc28bd192e0c53c0c686869fbf1a04
Grzesiek <g.pasko@gmail.com>
2023-05-15 06:43:30 +0200

    VariablesInitializer tests

---
+++ BenchmarkTests/Variables/VariablesInitializer.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkTests.Variables;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class VariablesInitializer
+{
+    public void EmptyArray()
+    {
+        var array = Array.Empty<int>();
+    }
+    
+    public void EmptyArrayWithSize()
+    {
+        var array = new int[0];
+    }
+    
+    public void EmptyList()
+    {
+        var list = new List<int>();
+    }
+    
+    public void EmptyListByEnumerable()
+    {
+        var list = Enumerable.Empty<int>();
+
+        list = new List<int>();
+
+    }
+
+}
---
+++ BenchmarkTests/Variables/VariablesInitializerBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkTests.Variables;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class VariablesInitializerBenchmarks
+{
+    private static readonly VariablesInitializer VariablesInitializer = new(); 
+        
+    
+    [Benchmark(Baseline = true)]
+    public void EmptyArray()
+    {
+        VariablesInitializer.EmptyArray();
+    }
+    
+    [Benchmark]
+    public void EmptyArrayWithSize()
+    {
+        VariablesInitializer.EmptyArrayWithSize();
+    }
+}
